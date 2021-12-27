a441b0b2236768996b1e56f32f0d940a23a5c0f1
techmo0 <fedor.borovik.06@mail.ru>
2021-12-27 11:30:51 +0500

    add mutex

--- threads.c
+++ threads.c
@@ -3,17 +3,35 @@
 #include<unistd.h>
 #include<pthread.h>
 
+pthread_mutex_t mutex;
+int a = 0;
 void* thr(){
   printf("%s\n","THREAAADS YEEEAh");
   sleep(3);
   printf("%s\n","ENDinG THREAAADS((" );
+  for(int i = 0; i < 1000000 ; i++) {
+    //pthread_mutex_lock(&mutex);
+    a++;
+    //pthread_mutex_unlock(&mutex);
+  }
 }
 
 int main(){
-  pthread_t t1, t2;
+  
+  pthread_t t1, t2, t3, t4;
+  pthread_mutex_init(&mutex, NULL);
+
   if(pthread_create(&t1, NULL, &thr, NULL) != 0) {return 1;}
   if(pthread_create(&t2, NULL, &thr, NULL) != 0) {return 2;}
-  if(pthread_join(t1, NULL) != 0) {return 3;}
-  if(pthread_join(t2, NULL) != 0) {return 4;}
+  if(pthread_create(&t3, NULL, &thr, NULL) != 0) {return 3;}
+  if(pthread_create(&t4, NULL, &thr, NULL) != 0) {return 4;}
+  if(pthread_join(t1, NULL) != 0) {return 5;}
+  if(pthread_join(t2, NULL) != 0) {return 6;}
+  if(pthread_join(t3, NULL) != 0) {return 7;}
+  if(pthread_join(t4, NULL) != 0) {return 8;}
+
+  pthread_mutex_destroy(&mutex);
+
+  printf("%i\n", a);
   return 0;
 }
